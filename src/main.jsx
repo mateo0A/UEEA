6b091cacf0a17ef85acda4e8f25362fc0351da2f
Henry <hg097870pq@gmail.com>
2025-06-08 15:50:49 -0500

    new actx

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import '../javascript/subMenu.js'
 import '../javascript/menuHover.js'
 import '../animations/menuPlace.js'
 import '../animations/contentMain.js'
+import '../styles/actividades.css'
+import '../animations/cardsActivities.js'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
